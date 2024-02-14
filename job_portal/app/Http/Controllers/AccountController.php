60c925bc2420fcf6ab6e99910048756d8edb82e6
dhriti24 <shah.dhriti@gmail.com>
2024-02-14 00:05:34 -0500

    Job portal login and registration page using laravel

---
+++ job_portal/app/Http/Controllers/AccountController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AccountController extends Controller
+{
+    public function registration()
+    {
+        return view('front.account.registration');
+    }
+
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success', 'Registration successful');
+
+            return response()->json(['status' => 'success', 'errors' => []]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()->toArray()
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        return view('front.account.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('account.profile');
+            } else {
+                return redirect()->route('account.login')->with('error', 'Invalid email or password');
+            }
+        } else {
+            return redirect()->route('account.login')->withErrors($validator)->withInput($request->only('email'));
+        }
+    }
+
+    public function profile()
+    {
+        return view('front.account.profile');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login');
+    }
+}
---
+++ job_portal/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::group(['account'], function () {
+
+    Route::group(['middleware' => 'guest'], function () {
+        Route::get('/account/register', [AccountController::class, 'registration'])->name('account.registration');
+        Route::post('/account/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+        Route::get('/account/login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('/account/authenticate', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('/account/profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::get('/account/logout', [AccountController::class, 'logout'])->name('account.logout');
+    });
+});
